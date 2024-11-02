53396da09d8ed4d0ab94c365cc38dfd300a5e495
coooo77 <coooo77@hotmail.com>
2024-11-02 14:29:43 +0800

    update package

--- @app/videoThumbnailCreator/src/createBat.ts
+++ @app/videoThumbnailCreator/src/createBat.ts
@@ -9,10 +9,8 @@ fs.writeFileSync(
   @echo off
   setlocal
 
-  REM 獲取當前批次檔案的執行位置
   set "current_dir=%~dp0"
 
-  REM 傳遞當前目錄作為參數給 Node.js 腳本
   node ${path.join(cwd(), 'dist/index.js')} %current_dir% --ext=mp4 --ext=ts --limit=5
 
   endlocal
